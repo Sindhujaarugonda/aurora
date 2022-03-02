c63f97edc34abeb96e1c36356192c3dbc088fbb5
Sindhujaarugonda <89595043+Sindhujaarugonda@users.noreply.github.com>
2022-03-02 23:51:27 +0530

    Add files via upload

---
+++ admissions.js
@@ -0,0 +1,8 @@
+function submission() {
+  document.getElementById("submit").alert ("Your Application was Successfull.Thank you!");
+}
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  alert("Hello World!");
+});
